7252981f200dab2a05c00bfd1943596913527e14
Shichao An <shichao.an@nyu.edu>
2015-04-16 01:38:30 -0700

    Added 3sum closest

---
+++ 3sum_closest/solution.rb
@@ -0,0 +1,29 @@
+class Solution
+  def threeSumClosest(num, target)
+    num.sort!
+    n = num.length
+    res = num[0] + num[1] + num[2]
+    (0..n - 3).each do |i|
+      l = i + 1
+      r = n - 1
+      while l < r
+        s = num[i] + num[l] + num[r]
+        res = s if (s - target).abs < (res - target).abs
+        if s == target
+          return s
+        elsif s < target
+          l += 1
+        else
+          r -= 1
+        end
+      end
+    end
+    res
+  end
+end
+
+num = [-4, 2, 4, 6, 11]
+target = 14
+s = Solution.new
+res = s.threeSumClosest num, target
+puts res
